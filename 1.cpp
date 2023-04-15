6457c420c1ce0d785d46ea1b177b48ff44e435b1
Ayush Singh <86451528+BeastBoyAsh@users.noreply.github.com>
2023-04-15 18:09:55 +0530

    Add files via upload

---
+++ 1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+main()
+{int a=12345,b;
+b=a%10;
+cout<<"Last is=" <<b; 
+a=a/10;
+b=a%10;
+cout<<"\nLast is=" <<b; 
+a=a/10;
+b=a%10;
+cout<<"\nLast is=" <<b; 
+a=a/10;
+b=a%10;
+cout<<"\nLast is=" <<b; 
+a=a/10;
+b=a%10;
+cout<<"\nLast is=" <<b; 
+
+}
